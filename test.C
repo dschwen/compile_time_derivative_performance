6cd0ede264fefa58c36bac886be9204ebf71ff47
Daniel Schwen <daniel.schwen@inl.gov>
2022-09-20 11:49:45 -0600

    Range based loop

--- test.C
+++ test.C
@@ -1,8 +1,59 @@
 #include "CompileTimeDerivatives.h"
 #include <chrono>
+#include <execution>
+#include <algorithm>
 
 using namespace libMesh;
 using namespace CompileTimeDerivatives;
+
+class FloatRange
+{
+public:
+  class iterator
+  {
+  public:
+    iterator(Real i, Real di) : _i(i), _di(di) {}
+
+    Real operator*() const { return _i; }
+
+    const iterator &operator++()
+    {
+      _i += _di;
+      return *this;
+    }
+
+    iterator operator++(int)
+    {
+      iterator returnval(*this);
+      ++_i;
+      return returnval;
+    }
+
+    bool operator==(const iterator &j) const
+    {
+      return (_i >= j._i);
+    }
+
+    bool operator!=(const iterator &j) const
+    {
+      return !(*this == j);
+    }
+
+  private:
+    Real _i, _di;
+  };
+
+  FloatRange(Real begin, Real end, Real step) : _begin(begin, step),
+                                                _end(end, step)
+  {
+  }
+
+  iterator begin() const { return _begin; }
+  iterator end() const { return _end; }
+
+private:
+  iterator _begin, _end;
+};
 
 int main()
 {
@@ -11,9 +62,7 @@ int main()
     dX
   };
 
-  Real v = 0.0;
-  const auto x = makeRef<dX>(v);
-  const auto result = x * (1.0 - x) - (x * log(x) + (1.0 - x) * log(1.0 - x));
+  const auto range = FloatRange(0.01, 0.99, 1e-9);
 
   Real r0 = 0, r1 = 0, r2 = 0;
   Real s0 = 0, s1 = 0, s2 = 0;
@@ -22,8 +71,12 @@ int main()
 
   // evaluate expression template
   start = std::chrono::system_clock::now();
-  for (v = 0.01; v <= 0.99; v += 1e-9)
+
+  for (auto v : range)
   {
+    const auto x = makeRef<dX>(v);
+    const auto result = x * (1.0 - x) - (x * log(x) + (1.0 - x) * log(1.0 - x));
+
     r0 += result();
     r1 += result.D<dX>()();
     r2 += result.D<dX>().D<dX>()();
@@ -33,8 +86,11 @@ int main()
 
   // evaluate native expression
   start = std::chrono::system_clock::now();
-  for (v = 0.01; v <= 0.99; v += 1e-9)
+  for (auto v : range)
   {
+    const auto x = makeRef<dX>(v);
+    const auto result = x * (1.0 - x) - (x * log(x) + (1.0 - x) * log(1.0 - x));
+
     s0 += v * (1.0 - v) - (v * std::log(v) + (1.0 - v) * std::log(1.0 - v));
     s1 += -2.0 * v - std::log(v) + std::log(1.0 - v) - (v - 1.0) / (1.0 - v);
     s2 += -2.0 + 1.0 / (v - 1.0) - 1.0 / v;
